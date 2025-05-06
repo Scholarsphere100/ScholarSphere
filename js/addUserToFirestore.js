eb77a7c11f6eb255e731feef8b8f7c1c917208cd
Simcita <mlisanasimamnkele10@gmail.com>
2025-05-06 23:54:22 +0200

    trying from scratch

--- js/addUserToFirestore.js
+++
@@ -1,9 +0,0 @@
-// js/addUserToFirestore.js
-
-async function addUserToFirestore(db, name, userId) {
-    // This will be overridden by the Jest mock in your test
-    return {};
-  }
-  
-  module.exports = { addUserToFirestore };
-  
