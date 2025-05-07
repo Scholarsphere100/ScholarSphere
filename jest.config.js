b598f63c1e9715f4978dc22b402a4e9862e7684c
Simcita <mlisanasimamnkele10@gmail.com>
2025-05-07 19:18:57 +0200

    can now find files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.js'],
+  testMatch: [
+    '<rootDir>/jest/tests/**/*.test.js',  // Points to your custom directory
+    '<rootDir>/jest/tests/**/*.spec.js'    // Optional: if you use .spec.js files
+  ],
+  moduleFileExtensions: ['js', 'jsx'],
+  testPathIgnorePatterns: ['/node_modules/'],
   verbose: true,
+  roots: ['<rootDir>/jest/tests']  // This helps Jest start looking from your custom directory
 };
